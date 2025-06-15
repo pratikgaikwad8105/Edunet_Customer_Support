7048255d6971f258869be7f8b8e3eb5518b6db20
Pratik <gaikwadpratik8105@gmail.com>
2025-06-15 09:49:28 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,40 @@
+from groq import Groq
+import os
+
+system_prompt = """
+                    You are a Customer Care Support representative for Edunet Foundation. Your role is to help students, participants, and stakeholders resolve issues, answer questions, and provide information related to Edunet Foundation's programs, initiatives, certifications, internships, workshops, or services.
+
+                        Your responses should be:
+                        - Polite, clear, helpful, and professional.
+                        - Strictly limited to Edunet Foundation's programs, initiatives, certifications, internships, workshops, services, or related support.
+                        - If a question falls outside your role or is not related to Edunet Foundation, respond by stating you can only help with questions related to Edunet Foundation's initiatives and services.
+
+                        Your main objective is to provide a helpful and supportive experience for anyone contacting you for Edunet Foundation-related inquiries.
+               """
+
+client = Groq(
+    api_key=os.getenv("GROQ_API_KEY"),
+)
+
+conversation_history = [
+    {"role": "system", "content": system_prompt}
+]
+
+
+def chat(prompt):
+    global conversation_history
+
+    conversation_history.append({"role": "user", "content": prompt})
+    chat_completion = client.chat.completions.create(
+        messages=conversation_history,
+        temperature=0.4,
+        top_p=0.9,
+        max_tokens=1024,
+        stop=["User :", "Assistant :"],
+
+        model="llama-3.1-8b-instant",
+    )
+
+    conversation_history.append({"role": "assistant", "content": chat_completion.choices[0].message.content})
+
+    return chat_completion
---
+++ ui.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import app
+
+st.set_page_config(
+    page_title="Customer Support For Edunet Foundation",
+    page_icon="👩‍⚕️"
+)
+
+st.title(" 💬 Customer Support For Edunet Foundation ")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+prompt = st.chat_input("Ask Me Anything!")
+
+
+if prompt:
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    response = app.chat(prompt).choices[0].message.content
+
+    with st.chat_message("assistant"):
+        st.markdown(response)
+
+    st.session_state.messages.append({"role": "assistant", "content": response})
